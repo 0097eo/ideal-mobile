c976bcdbcf0ab0572a6cc7f0623b3a1a32d07726
0097eo <emmanuelokello294@gmail.com>
2025-06-11 12:12:13 +0300

    refactor layout components; integrate SafeAreaProvider for improved safe area handling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,57 +2,60 @@ import { ProtectedRoute } from "@/context/ProtectedRoute"
 import { Tabs } from "expo-router"
 import { Home, ShoppingBag, ShoppingCart, Heart, User} from 'lucide-react-native'
 import { StatusBar } from "expo-status-bar";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function TabLayout() {
-    return (
-        <ProtectedRoute>
-            <StatusBar style="auto" />
-            <Tabs screenOptions={{
-                tabBarActiveTintColor: '#f59e0b',
-                tabBarInactiveTintColor: 'gray',
-                tabBarStyle: {
-                    paddingBottom: 5,
-                    height: 60,
-                },
-                }}>
-                <Tabs.Screen
-                    name="index"
-                    options={{
-                        title: 'Home',
-                        tabBarIcon: ({ color }) => <Home size={24} color={color} />,
-                    }}
-                />
-                <Tabs.Screen
-                    name="shop"
-                    options={{
-                        title: 'Shop',
-                        tabBarIcon: ({ color }) => <ShoppingBag size={24} color={color} />,
-                    }}
-                />
-                <Tabs.Screen
-                    name="cart"
-                    options={{
-                        title: 'Cart',
-                        tabBarIcon: ({ color }) => <ShoppingCart size={24} color={color} />,
-                    }}
-                />
-                <Tabs.Screen
-                    name="wishlist"
-                    options={{
-                        title: 'Wishlist',
-                        tabBarIcon: ({ color }) => <Heart size={24} color={color} />,
-                    }}
-                />
-                <Tabs.Screen
-                    name="account"
-                    options={{
-                        title: 'Account',
-                        tabBarIcon: ({ color }) => <User size={24} color={color} />,
-                    }}
-                />
-            </Tabs>
-        </ProtectedRoute>
+  const insets = useSafeAreaInsets();
 
-    )
-
+  return (
+    <ProtectedRoute>
+      <StatusBar style="auto" />
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#f59e0b",
+          tabBarInactiveTintColor: "gray",
+          tabBarStyle: {
+            paddingBottom: Math.max(insets.bottom, 5),
+            height: 60 + insets.bottom,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => <Home size={24} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="shop"
+          options={{
+            title: "Shop",
+            tabBarIcon: ({ color }) => <ShoppingBag size={24} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="cart"
+          options={{
+            title: "Cart",
+            tabBarIcon: ({ color }) => <ShoppingCart size={24} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="wishlist"
+          options={{
+            title: "Wishlist",
+            tabBarIcon: ({ color }) => <Heart size={24} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="account"
+          options={{
+            title: "Account",
+            tabBarIcon: ({ color }) => <User size={24} color={color} />,
+          }}
+        />
+      </Tabs>
+    </ProtectedRoute>
+  );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,17 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "@/context/AuthContext";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 
 export default function RootLayout() {
   return (
   <AuthProvider>
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
   </AuthProvider>
   ) 
 }
