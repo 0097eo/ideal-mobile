cc0cc892f28ad31df32b4a5b764a3330e740957f
0097eo <emmanuelokello294@gmail.com>
2026-03-05 20:45:38 +0300

    fix: cleartext traffic, env variables, svg dependency

--- constants/api.ts
+++ constants/api.ts
@@ -1,14 +1,1 @@
-
-const getApiUrl = () => {
-  if (__DEV__) {
-    const HOST_IP = '192.168.0.107';
-    const PORT = '8000';
-    
-    return `http://${HOST_IP}:${PORT}/`;
-  } else {
-    // Production URL
-    return 'https://production-domain.com/';
-  }
-};
-
-export const API_URL = getApiUrl();
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'https://production-domain.com';
